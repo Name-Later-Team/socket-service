7843486c0a2bd4c24954880c619342fbc787e16f
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2023-04-12 23:28:32 +0700

    change solution for saving redis key

--- src/controllers/tickets.controller.js
+++ src/controllers/tickets.controller.js
@@ -5,17 +5,25 @@ import { HTTP_CODE } from "../common/constants/response-code.const.js";
 import { ResponseBuilder } from "../common/utils/builders/response.builder.js";
 import RedisClient from "../infrastruture/connections/redis.js";
 
-export const createTicketHeaderSchema = Joi.object({
-    "x-request-id": Joi.string().uuid().required().messages({
-        "string.base": "Request ID phải là chuỗi",
-        "string.empty": "Request ID không được bỏ trống",
-        "string.guid": "Request ID phải là uuid",
-        "any.required": "Request ID không được bỏ trống",
-    }),
-    "service-slug": Joi.string().required().messages({
-        "string.base": "Service Slug phải là chuỗi",
-        "string.empty": "Service Slug không được bỏ trống",
-        "any.required": "Service Slug không được bỏ trống",
+// export const createTicketHeaderSchema = Joi.object({
+//     "x-request-id": Joi.string().uuid().required().messages({
+//         "string.base": "Request ID phải là chuỗi",
+//         "string.empty": "Request ID không được bỏ trống",
+//         "string.guid": "Request ID phải là uuid",
+//         "any.required": "Request ID không được bỏ trống",
+//     }),
+//     "service-slug": Joi.string().required().messages({
+//         "string.base": "Service Slug phải là chuỗi",
+//         "string.empty": "Service Slug không được bỏ trống",
+//         "any.required": "Service Slug không được bỏ trống",
+//     }),
+// }).options({ allowUnknown: true, stripUnknown: false });
+
+export const createTicketBodySchema = Joi.object({
+    userIdentifier: Joi.string().required().messages({
+        "string.base": "Mã người dùng phải là chuỗi",
+        "string.empty": "Mã người dùng không được bỏ trống",
+        "any.required": "Mã người dùng không được bỏ trống",
     }),
 }).options({ allowUnknown: true, stripUnknown: false });
 
@@ -30,16 +38,17 @@ export async function generateAuthenticationTicketAsync(req, res) {
     const ticket = randomBuffer.toString("hex");
 
     // get indentity information from header
-    const requestId = req.header("X-Request-Id");
-    const serviceSlug = req.header("Service-Slug") || "";
-    const requestIp = req.ip;
+    const { userIdentifier } = req.body;
+    // const serviceSlug = req.header("Service-Slug") || "";
+    // const requestIp = req.ip;
 
     // key will be set as a namespace, ie: "presento:tickets:82d886a9-cb7d-403d-9b9b-57c8d48bbf3e"
-    const redisKey = `${serviceSlug}:${TICKET_CONST.redisNamespace}:${requestId}`;
+    // const redisKey = `${serviceSlug}:${TICKET_CONST.redisNamespace}:${requestId}`;
+    const redisKey = `${TICKET_CONST.redisNamespace}:${userIdentifier}`;
 
     // save to redis
     // save data with built-in expires mechanism
-    await RedisClient.getRedisClient().setEx(redisKey, TICKET_CONST.timeToLive, JSON.stringify({ ticket, requestIp }));
+    await RedisClient.getRedisClient().setEx(redisKey, TICKET_CONST.timeToLive, ticket);
 
     res.status(HTTP_CODE.created).json(new ResponseBuilder().withCode(HTTP_CODE.created).withData({ ticket }).build());
 }
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -3,20 +3,20 @@ import { asyncRouteHandler } from "../common/middlewares/async-route.handler.js"
 import { introspectionMiddleware } from "../common/middlewares/introspection.middleware.js";
 import { requestValidationMiddleware } from "../common/middlewares/request-validation.middleware.js";
 import { rsaValidationMiddleware } from "../common/middlewares/rsa-validation.middleware.js";
-import { createTicketHeaderSchema, generateAuthenticationTicketAsync } from "../controllers/tickets.controller.js";
+import { createTicketBodySchema, generateAuthenticationTicketAsync } from "../controllers/tickets.controller.js";
 
 export const router = express.Router();
 
 router.post(
     "/tickets",
     introspectionMiddleware(),
-    requestValidationMiddleware("headers", createTicketHeaderSchema),
+    requestValidationMiddleware("body", createTicketBodySchema),
     asyncRouteHandler(generateAuthenticationTicketAsync),
 );
 
 router.post(
     "/audience/tickets",
     rsaValidationMiddleware(),
-    requestValidationMiddleware("headers", createTicketHeaderSchema),
+    requestValidationMiddleware("body", createTicketBodySchema),
     asyncRouteHandler(generateAuthenticationTicketAsync),
 );
